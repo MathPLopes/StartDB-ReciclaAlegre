89921aad10c7c02bb3233bd496c166dcabb3b26e
Dutkercz <dutkercz@gmail.com>
2025-12-02 18:52:00 -0300

    Repository de UsuarioGerador

---
+++ backend/reciclaalegre/src/main/java/db/start/reciclaalegre/repository/UsuarioGeradorRepository.java
@@ -0,0 +1,9 @@
+package db.start.reciclaalegre.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import db.start.reciclaalegre.model.UsuarioGerador;
+
+public interface UsuarioGeradorRepository extends JpaRepository<UsuarioGerador, Long>{
+
+}
