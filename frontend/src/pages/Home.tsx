f131664dd7ebd44ccef2bb9e38b178b8ceb5be9a
Dutkercz <dutkercz@gmail.com>
2026-01-21 11:52:19 -0300

    Refatorando Home.tsx, trocando link por NavLink (mostra em qual guia esta), alem de logica para mostrar o link perfil ou login/cadasto se usuario estiver desconectado

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,43 +1,63 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Card } from '../components/Card';
+import React, { useContext, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Card } from "../components/Card";
 import "../assets/style.css";
+import { AuthContext } from "../components/auth/AuthContext";
 
 const Home: React.FC = () => {
+  const auth = useContext(AuthContext);
 
-    useEffect(() => {
-        const toggleBtn = document.getElementById("toggle-theme");
-        if (toggleBtn) {
-            toggleBtn.addEventListener("click", () => {
-                document.body.classList.toggle("dark");
-            });
-        }
-    }, []);
-   
+  useEffect(() => {
+    const toggleBtn = document.getElementById("toggle-theme");
+    if (toggleBtn) {
+      toggleBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+      });
+    }
+  }, []);
 
-    return (
-        <div className="home-body">
+  return (
+    <div className="home-body">
+      <header>
+        <div className="logo">ReciclAlegre</div>
 
-            <header>
-                <div className="logo">ReciclAlegre</div>
+        <nav>
+          {auth?.user ? (
+            <>
+              <NavLink to="/Perfil" style={{ fontWeight: "bold" }}>
+                Olá, {auth?.user.perfil.nome || "Usuario"}
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink to="/login">Login</NavLink>
+              <NavLink to="/cadastro">Cadastro</NavLink>
+            </>
+          )}
+          <NavLink to="/sobre">Sobre</NavLink>
+        </nav>
 
-                <nav>
-                    <Link to="/login">Login</Link>
-                    <Link to="/cadastro">Cadastro</Link>
-                    <Link to="/sobre">Sobre</Link>
-                </nav>
+        <button id="toggle-theme" className="btn-theme">
+          🌙
+        </button>
+      </header>
 
-                <button id="toggle-theme" className="btn-theme">🌙</button>
-            </header>
-
-             <section className="grid-3">
-            <Card title="Coleta rápida" text="Solicite a coleta dos seus resíduos em minutos." />
-            <Card title="Reciclagem inteligente" text="Seu lixo ganha um novo destino sustentável." />
-            <Card title="Acompanhe tudo" text="Veja seu impacto positivo no meio ambiente." />
-</section>
-
-        </div>
-    );
+      <section className="grid-3">
+        <Card
+          title="Coleta rápida"
+          text="Solicite a coleta dos seus resíduos em minutos."
+        />
+        <Card
+          title="Reciclagem inteligente"
+          text="Seu lixo ganha um novo destino sustentável."
+        />
+        <Card
+          title="Acompanhe tudo"
+          text="Veja seu impacto positivo no meio ambiente."
+        />
+      </section>
+    </div>
+  );
 };
 
 export default Home;
